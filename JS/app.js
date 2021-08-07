8d351cf0332c5ce4b36903705870982ec20f133a
aryan <shaliaryan75@gmail.com>
2021-08-07 18:55:30 +0430

    finish coding of menu part && fix some bugs in hover effect

---
+++ JS/app.js
@@ -0,0 +1,59 @@
+// varible of menu item
+let o1 = document.getElementById("o1");
+let o2 = document.getElementById("o2");
+let o3 = document.getElementById("o3");
+let o4 = document.getElementById("o4");
+let o5 = document.getElementById("o5");
+// varible of menu item red part
+let sp1 = document.getElementById("sp1");
+let sp2 = document.getElementById("sp2");
+let sp3 = document.getElementById("sp3");
+let sp4 = document.getElementById("sp4");
+let sp5 = document.getElementById("sp5");
+//hover func
+function over1() {
+    sp1.style.opacity = 100;
+
+}
+
+function onover1() {
+    sp1.style.opacity = 0;
+}
+
+function over2() {
+    sp2.style.opacity = 100;
+
+}
+
+function onover2() {
+    sp2.style.opacity = 0;
+}
+
+function over3() {
+    sp3.style.opacity = 100;
+
+}
+
+function onover3() {
+    sp3.style.opacity = 0;
+}
+
+function over4() {
+    sp4.style.opacity = 100;
+
+}
+
+function onover4() {
+    sp4.style.opacity = 0;
+}
+
+function over5() {
+    sp5.style.opacity = 100;
+
+}
+
+function onover5() {
+    sp5.style.opacity = 0;
+}
+
+//contain part
